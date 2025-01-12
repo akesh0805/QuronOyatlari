77e376948cfc3f1061ab92198ac654ffc90defa3
akesh0805 <akesh0805@users.noreply.github.com>
2025-01-12 10:46:56 +0000

    deploy: 15c98b1b9a987bbf3851d2674aa028fe2eaaa072

---
+++ js/audioPlayer.js
@@ -0,0 +1,8 @@
+window.audioPlayer = {  
+    play: function (audioElement) {  
+        audioElement.play();  
+    },  
+    pause: function (audioElement) {  
+        audioElement.pause();  
+    }  
+};
