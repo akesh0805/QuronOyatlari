e8543aafa6cafe1f3f913c53dd32c27715dd4763
akesh0805 <vchd4.texnolog@gmail.com>
2025-01-13 13:47:19 +0500

    audio fixed

--- QuronOyatlari/wwwroot/js/audioPlayer.js
+++ QuronOyatlari/wwwroot/js/audioPlayer.js
@@ -1,8 +1,19 @@
-window.audioPlayer = {  
-    play: function (audioElement) {  
-        audioElement.play();  
-    },  
-    pause: function (audioElement) {  
-        audioElement.pause();  
-    }  
+window.audioPlayer = {
+    setupAudioEndedHandler: function (audioElement, dotNetReference) {
+        audioElement.addEventListener('ended', function () {
+            dotNetReference.invokeMethodAsync('OnAudioEnded');
+        });
+    },
+
+    play: function (audioElement) {
+        audioElement.play();
+    },
+
+    pause: function (audioElement) {
+        audioElement.pause();
+    },
+
+    setCurrentTime: function (audioElement, time) {
+        audioElement.currentTime = time;
+    }
 };
